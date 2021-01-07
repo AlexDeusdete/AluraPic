da39cee70d237ac6a946fd5e7c59f3f1828a18ff
AlexDeusdete <alexdeusdete@hotmail.com>
2021-01-06 22:47:30 -0300

    ajuste no scopo dos serviços

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,11 +2,13 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+
 import { VMessageModule } from '../shared/components/vmessage/vmessage.module';
 import { HomeComponent } from './home.component';
 import { HomeRoutingModule } from './home.routing.module';
 import { SignInComponent } from './signin/signin.component';
 import { SignUpComponent } from './signup/signup.component';
+import { SignUpService } from './signup/signup.service';
 
 @NgModule({
     declarations: [ 
@@ -19,6 +21,9 @@ import { SignUpComponent } from './signup/signup.component';
               VMessageModule,
               RouterModule,
               FormsModule,
-              HomeRoutingModule]
+              HomeRoutingModule],
+    providers: [
+        SignUpService
+    ]
 })
 export class HomeModule { }
