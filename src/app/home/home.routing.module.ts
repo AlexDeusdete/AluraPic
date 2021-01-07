f3c19c3e3702b9197e4cd737d17798239a2f7f7a
AlexDeusdete <alexdeusdete@hotmail.com>
2021-01-07 16:21:45 -0300

    alteração na autenticação e acesso

--- src/app/home/home.routing.module.ts
+++ src/app/home/home.routing.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { AuthGuard } from '../core/auth/auth.guard';
+import { LoginGuard } from '../core/auth/login.guard';
 import { HomeComponent } from './home.component';
 import { SignInComponent } from './signin/signin.component';
 
 const routes: Routes = [
   { path: '',
     component: HomeComponent,
-    canActivate: [AuthGuard],
+    canActivate: [LoginGuard],
     children: [
       { 
         path: '',
