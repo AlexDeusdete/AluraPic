3679b843bdfc79103767d7ae0606b5121cddbd5d
AlexDeusdete <alexdeusdete@hotmail.com>
2021-01-08 23:13:41 -0300

    ciação de um componente de mensagem

--- src/app/photos/photo-details/photo-details.component.ts
+++ src/app/photos/photo-details/photo-details.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from "@angular/core";
 import { ActivatedRoute, Router } from '@angular/router';
 import { Observable } from 'rxjs';
+import { UserService } from 'src/app/core/user/user.service';
+import { AlertService } from 'src/app/shared/components/alert/alert.service';
 
 import { Photo } from '../photo/photo';
 import { PhotoService } from '../photo/photo.service';
@@ -16,7 +18,9 @@ export class PhotoDetailsComponent implements OnInit {
     constructor(
         private route: ActivatedRoute,
         private photoService: PhotoService,
-        private router: Router) {}
+        private router: Router,
+        private alertService: AlertService,
+        private userService: UserService) {}
 
     ngOnInit(): void {
         this.photoId = this.route.snapshot.params.photoId;
@@ -26,6 +30,12 @@ export class PhotoDetailsComponent implements OnInit {
 
     remove(){
         this.photoService.removePhoto(this.photoId)
-            .subscribe(() => this.router.navigate(['']));
+            .subscribe(
+                () => {
+                    this.alertService.success("Photo removed!", true)
+                    this.router.navigate(['/user', this.userService.getUserName()]);                
+                },
+                err => this.alertService.danger(err.message, true)
+            );
     }
 }
