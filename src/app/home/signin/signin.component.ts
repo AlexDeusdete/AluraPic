3726357abf09042161e6555c48d30f55e0f84481
AlexDeusdete <alexdeusdete@hotmail.com>
2021-01-09 23:15:05 -0300

    url de callback no signin

--- src/app/home/signin/signin.component.ts
+++ src/app/home/signin/signin.component.ts
@@ -1,7 +1,7 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Title } from '@angular/platform-browser';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 import { AuthService } from 'src/app/core/auth/auth.service';
 import { PlatformDetectorService } from 'src/app/core/platform-detector/platform-detector.service';
@@ -11,6 +11,7 @@ import { PlatformDetectorService } from 'src/app/core/platform-detector/platform
 })
 export class SignInComponent implements OnInit {
 
+    fromUrl: string;
     loginForm: FormGroup;
     @ViewChild('userNameInput') userNameInput: ElementRef<HTMLInputElement>;
 
@@ -18,10 +19,15 @@ export class SignInComponent implements OnInit {
         private formBuilder: FormBuilder,
         private authService: AuthService,
         private router: Router,
-        private platformDetectorService: PlatformDetectorService
+        private platformDetectorService: PlatformDetectorService,
+        private activatedRoute: ActivatedRoute
     ){ }
 
     ngOnInit(): void {
+        this.activatedRoute
+            .queryParams
+            .subscribe(params => this.fromUrl = params.fromUrl);
+
         this.loginForm = this.formBuilder.group({
             userName: ['', Validators.required],
             password: ['', Validators.required]
@@ -34,7 +40,10 @@ export class SignInComponent implements OnInit {
 
         this.authService.authenticate(userName, password)
             .subscribe(
-                () => this.router.navigate(['user', userName]),
+                () => this.fromUrl 
+                      ? this.router.navigateByUrl(this.fromUrl)
+                      : this.router.navigate(['user', userName])  
+                ,
                 err => {
                     this.loginForm.reset();
                     this.platformDetectorService.isPlatformBrowser() &&
